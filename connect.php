3907e48910c26055e38625836933d06717fbed65
Nguyen Kim Huong <sakura25072022@gmail.com>
2025-11-24 09:30:15 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,7 +7,7 @@ $database = "da_ltw";
 $conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
-    die("Không thể kết nối database: " . mysqli_connect_error());
+    die("Không thể kết nối dữ liệu: ".mysqli_connect-error());
 }
 mysqli_query($conn, "SET NAMES 'utf8'");
 ?>
